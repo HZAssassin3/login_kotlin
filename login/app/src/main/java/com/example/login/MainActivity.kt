9c9c1364da11ff5e784e066a41cbd6558c1ade5d
İsmail Burunağıl <gskayserigs38gs@gmail.com>
2024-03-10 03:22:23 +0300

    Initial commit

---
+++ login/app/src/main/java/com/example/login/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.login
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.login.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.loginBtn.setOnClickListener{
+            val userName =  binding.usermaneInput.text.toString()
+            val password =  binding.passwordInput.text.toString()
+            Log.i("Test Credentials","Username : $userName and Password : $password")
+        }
+    }
+}
